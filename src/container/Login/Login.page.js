944126eacabb992f6981236a1b4d36907a148ace
unknown <syraxrakotonarivo@gmail.com>
2020-12-23 07:46:37 +0300

    navbar

--- src/container/Login/Login.page.js
+++ src/container/Login/Login.page.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './Login.style.scss';
+import NavBar from '../../component/NavBar/NavBar.page';
 
 const Login = () => {
   return (
     <>
-      
+      <NavBar />
     </>
   );
 }
